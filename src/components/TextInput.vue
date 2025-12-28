f152e299de4b7b6ee18cfe0f5f9b1087a3dc5590
Mavv3006 <marvdeus@gmail.com>
2025-12-28 18:02:24 +0100

    init

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -1,26 +1,26 @@
 <script setup>
 import { onMounted, ref } from "vue";
 
-defineProps(['modelValue']);
+defineProps(["modelValue"]);
 
-defineEmits(['update:modelValue']);
+defineEmits(["update:modelValue"]);
 
 const input = ref(null);
 
 onMounted(() => {
-    if (input.value.hasAttribute('autofocus')) {
-        input.value.focus();
-    }
+  if (input.value.hasAttribute("autofocus")) {
+    input.value.focus();
+  }
 });
 
-defineExpose({focus: () => input.value.focus()});
+defineExpose({ focus: () => input.value.focus() });
 </script>
 
 <template>
-    <input
-        class="border-gray-300 focus:border-[#041286] focus:ring-[#041286] rounded-md shadow-xs disabled:text-gray-500"
-        :value="modelValue"
-        @input="$emit('update:modelValue', $event.target.value)"
-        ref="input"
-    />
+  <input
+    class="border-gray-300 focus:border-[#041286] focus:ring-[#041286] rounded-md shadow-xs disabled:text-gray-500"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    ref="input"
+  />
 </template>
