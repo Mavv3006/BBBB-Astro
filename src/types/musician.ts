0f9b64a874c17808c88fc0d3bd054740e74e3ddb
Mavv3006 <marvdeus@gmail.com>
2025-12-28 22:25:32 +0100

    updated newsletter.astro

---
+++ src/types/musician.ts
@@ -0,0 +1,30 @@
+export type MusicianBackendDto = {
+    id: number,
+    instrument_id: number,
+    firstname: string,
+    lastname: string,
+    seating_position?: number,
+    picture_filepath?: string
+}
+
+export type MusicianProp = {
+    instrument: {
+        id: number,
+        name: string,
+        default_picture_filepath: string,
+        tux_filepath: string,
+    },
+    musicians: MusicianBackendDto[]
+}
+
+export type ReducedInstrument = {
+    name: string
+    default_picture_filepath: string,
+};
+
+export type MusicianWithInstrument = {
+    instrument: ReducedInstrument,
+    musicians: Musician[]
+}
+
+export type Musician = { name: String };
